5482403311df0d9eaaa3716851c0159705db14e1
dev-summer <summerly812@gmail.com>
2022-07-18 19:39:16 +0900

    LeetCode 20

---
+++ LeetCode/LeetCode20.swift
@@ -0,0 +1,30 @@
+// LeetCode 20 Valid Parentheses
+
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        var charArr = Array(s)
+        var stack = [Character]()
+        
+        var dic: Dictionary<Character, Character> = ["(": ")", "{": "}", "[": "]"]
+        
+        for char in charArr {
+            if char == "(" || char == "{" || char == "[" {
+                stack.append(char)
+            } else {
+                if stack.isEmpty { return false }
+                let last = stack.last!
+                if dic[last]! == char {
+                    stack.removeLast()
+                } else {
+                    return false
+                }
+            }
+        }
+        
+        if stack.isEmpty {
+            return true
+        } else {
+            return false
+        }
+    }
+}
