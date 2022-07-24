ab4c67edbac2d47a80950d0806a840d9ab822f76
dev-summer <summerly812@gmail.com>
2022-07-24 23:25:07 +0900

    BOJ 15665

---
+++ BOJ/BOJ14888.swift
@@ -0,0 +1,47 @@
+// BOJ 15665 N과 M(11)
+// N개의 자연수 중에서 M개를 고른 수열, 중복 허용
+
+// 배열을 사용해서 재귀하면 시간 초과가 발생
+// 시간 초과가 발생하면 str을 사용해보자
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0]
+let m = input[1]
+
+var numbers = readLine()!.split(separator: " ").map { Int($0)! }
+numbers.sort()
+
+var arr = [Int]()
+
+for num in numbers {
+    if arr.contains(num) {
+        continue
+    } else {
+        arr.append(num)
+    }
+}
+
+var result = [Int]()
+
+func dfs(_ count: Int, _ str: String) {
+    if count == m {
+//        results.append(result)
+        print(str)
+        return
+    }
+    
+    for i in 0..<arr.count {
+        var str = str
+        str.append("\(arr[i]) ")
+        dfs(count + 1, str)
+    }
+}
+
+dfs(0, "")
+
+//for line in results {
+//    for i in line {
+//        print(i, terminator: " ")
+//    }
+//    print()
+//}
--- main.swift
+++ main.swift
@@ -1,1 +1,31 @@
+let numbers = Int(readLine()!)!
+let numArr = readLine()!.split(separator: " ").map { Int($0)! }
+var symbols = readLine()!.split(separator: " ").map { Int($0)! }
 
+func recur(_ sum: Int, _ idx: Int) {
+    var sum = sum
+    for i in 0..<4 {
+        if symbols[i] > 0 {
+            symbols[i] -= 1
+            if i == 0 {
+                sum += numArr[idx]
+                recur(sum, idx + 1)
+            } else if i == 1 {
+                sum -= numArr[idx]
+                recur(sum, idx + 1)
+            } else if i == 2 {
+                sum *= numArr[idx]
+                recur(sum, idx + 1)
+            } else if i == 3 {
+                if sum < 0 {
+                    var temp = -sum
+                    temp /= numArr[idx]
+                    sum = -temp
+                    recur(sum, idx + 1)
+                }
+            }
+        }
+    }
+}
+
+recur(numArr[0], 1)
